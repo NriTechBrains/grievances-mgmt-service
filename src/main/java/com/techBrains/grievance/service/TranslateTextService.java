261ff316ec51b42d87c1985e7b300beec08313d1
NriTechBrains <nritechbrainsfortdp2024@gmail.com>
2024-07-20 14:09:47 -0500

    Env variables

--- src/main/java/com/techBrains/grievance/service/TranslateTextService.java
+++ src/main/java/com/techBrains/grievance/service/TranslateTextService.java
@@ -5,17 +5,22 @@ import com.google.cloud.translate.TranslateOptions;
 
 import com.google.cloud.translate.Translation;
 import com.techBrains.grievance.exception.MessageTranslationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Service
 public class TranslateTextService {
+
+    @Value("${googleApiKey}")
+    private String googleApiKey;
+
     public String translateMessage(String message) {
 
         try {
             Translate translate = TranslateOptions.newBuilder()
-                    .setApiKey("AIzaSyB-z5RXVHhFC-1s2V947BHqFOE4QNree6Y").build().getService();
+                    .setApiKey(googleApiKey).build().getService();
             Translation translation = translate.translate(message,
                     Translate.TranslateOption.sourceLanguage("en"),
                     Translate.TranslateOption.targetLanguage("te"));
